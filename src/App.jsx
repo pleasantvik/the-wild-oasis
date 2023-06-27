43dc6c4edff48276c699639b5e51dbfc8cc32d26
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-06-27 18:53:15 +0100

    installation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #bf4f74;
+`;
+function App() {
+  return <Title>hello hi</Title>;
+}
+
+export default App;
